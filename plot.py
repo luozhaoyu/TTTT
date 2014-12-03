c510b352ebda046daff8a4c0d79d9f2f633d9692
Zhaoyu Luo <zhaoyu@cs.wisc.edu>
2014-12-03 01:58:54 -0600

    almost finished plotting

--- plot.py
+++ plot.py
@@ -10,6 +10,9 @@ import os
 import datetime
 import random
 import time
+import re
+
+import argparse
 
 import numpy
 from matplotlib import pyplot
@@ -51,6 +54,9 @@ class DstatPlot(object):
         self.sp.legend()
         return self.fig.show()
 
+    def savefig(self, file_path):
+        return self.fig.savefig(file_path)
+
 
 def foo():
     dp = DstatPlot()
@@ -58,10 +64,143 @@ def foo():
     return dp
 
 
+def parse(file_path):
+    d = {}
+    key = ''
+    with open(file_path, 'r') as f:
+        for line in f:
+            content = line.strip()
+            if (line.startswith("\t") or line.startswith(" ")) and\
+                not (content.startswith('at') or content.startswith('.')):
+                if "=" in content:
+                    name, value = content.split('=')
+                    d[key][name] = int(value)
+                else:
+                    key = content
+                    d[key] = {}
+    return d
+
+
+def parse_folder(folder_path):
+    folder_path = os.path.expanduser(folder_path)
+    d = {}
+    for i in os.listdir(folder_path):
+        nodes = int(re.search(r'\d+', i).group())
+        p = parse(os.path.join(folder_path, i))
+        if p:
+            d[nodes] = p
+    return d
+
+
+def plot_variance(folder_path):
+    d = parse_folder(folder_path)
+    x = d.keys()
+    x.sort()
+    ys = []
+    titles = ['MapCount', 'ReduceCount', 'MapDuration', 'ReduceDuration']
+    for t in titles:
+        ys.append([numpy.std(d[n][t].values()) for n in x])
+    fig = pyplot.figure()
+    for i in range(len(titles)):
+        y = ys[i]
+        title = titles[i]
+        sp = fig.add_subplot(2, 2, i)
+        sp.set_title(title)
+        sp.set_xlabel('number of nodes')
+        sp.set_ylabel('standard deviation')
+        sp.bar(numpy.arange(len(x)), y)
+        sp.set_xticks(numpy.arange(len(x)))
+        sp.set_xticklabels(x)
+    fig.savefig('variance.png')
+    return fig
+
+
+def plot_single(folder_path):
+    d = parse_folder(folder_path)
+    fig = pyplot.figure()
+    for i, n in enumerate(d.keys()):
+        sp = fig.add_subplot(len(d), 1, 1 + i)
+        sp.set_title("%i hosts" % n)
+        sp.set_xlabel('hosts names')
+        sp.set_ylabel('milliseconds')
+        x = [i.split('.')[0] for i in d[n]['MapDuration'].keys()]
+        y = d[n]['MapDuration'].values()
+        sp.bar(numpy.arange(len(x)), y)
+        sp.set_xticks(numpy.arange(len(x)))
+        sp.set_xticklabels(x)
+    fig.savefig('single.png')
+    return fig
+
+
+def plot_time(folder_path):
+    d = parse_folder(folder_path)
+    x = d.keys()
+    x.sort()
+    map_times = []
+    reduce_times = []
+    total_times = []
+    for i in x:
+        mt = d[i]['Job Counters']['Total time spent by all map tasks (ms)']
+        rt = d[i]['Job Counters']['Total time spent by all reduce tasks (ms)']
+        map_times.append(mt)
+        reduce_times.append(rt)
+        total_times.append(mt+rt)
+    fig = pyplot.figure()
+    sp = fig.add_subplot(3, 1, 1)
+    sp.set_title("Map Time Cost")
+    sp.set_xlabel('number of nodes')
+    sp.set_ylabel('milliseconds')
+    sp.plot(numpy.arange(len(x)), map_times)
+    sp.set_xticks(numpy.arange(len(x)))
+    sp.set_xticklabels(x)
+    sp1 = fig.add_subplot(3, 1, 2)
+    sp1.set_title("Reduce Time Cost")
+    sp1.set_xlabel('number of nodes')
+    sp1.set_ylabel('milliseconds')
+    sp1.plot(numpy.arange(len(x)), reduce_times)
+    sp1.set_xticks(numpy.arange(len(x)))
+    sp1.set_xticklabels(x)
+    sp2 = fig.add_subplot(3, 1, 3)
+    sp2.set_title("Total Time Cost")
+    sp2.set_xlabel('number of nodes')
+    sp2.set_ylabel('milliseconds')
+    sp2.plot(numpy.arange(len(x)), total_times)
+    sp2.set_xticks(numpy.arange(len(x)))
+    sp2.set_xticklabels(x)
+    fig.savefig('time.png')
+    return fig
+
+
 def main(argv):
+    import pprint
     import time
-    d = foo()
-    d.show()
+    parser = argparse.ArgumentParser(description="""
+        Plots for TTTT""")
+    different_actions = parser.add_mutually_exclusive_group()
+    different_actions.add_argument('-d', help='draw from dstat')
+    different_actions.add_argument('-v', help='plot variance', action='store_true')
+    different_actions.add_argument('-t', help='plot time', action='store_true')
+    different_actions.add_argument('-s', help='plot each single node time spent', action='store_true')
+    different_actions.add_argument('--test', help='', action="store_true")
+    parser.add_argument('--file')
+    parser.add_argument('--folder', default="./experiments/")
+    args = parser.parse_args()
+    if args.d:
+        dp = DstatPlot()
+        dp.plot_folder()
+        dp.savefig('dstat.png')
+        dp.show()
+    elif args.v:
+        plot_variance(args.folder).show()
+    elif args.t:
+        plot_time(args.folder).show()
+    elif args.s:
+        plot_single(args.folder).show()
+    elif args.test:
+        if args.file:
+            pprint.pprint(parse(args.file))
+        else:
+            pprint.pprint(plot_variance(args.folder))
     while True:
         time.sleep(1)
 
